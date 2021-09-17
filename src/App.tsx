a1ce49a894cec062110a5e013523d48a217e561c
Adrian <adrknapp@gmail.com>
2021-09-16 22:53:37 -0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,7 @@ export function App() {
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <SideBar genres={genres} handleClickButton={handleClickButton}/>
+      <SideBar genres={genres}/>
       <Content movies={movies} selectedGenre={selectedGenre}/>
     </div>
   )
